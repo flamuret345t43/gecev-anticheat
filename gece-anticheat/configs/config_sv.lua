35ecc06bb0e6747fd58e741d735521cf8b76247a
flamuret345t43 <73041398+flamuret345t43@users.noreply.github.com>
2021-12-29 19:35:42 +0100

    Add files via upload

---
+++ gece-anticheat/configs/config_sv.lua
@@ -0,0 +1,13 @@
+ConfigACS = {}
+
+ConfigACS.ModelsLogWebhook = "https://discord.com/api/webhooks/925808944499290143/GMYxW-i2gBCnHusHNxVDg1WBFOYXbD04GLt2FhMg7B5ViFqhHZ3yQXSwM5ZG6-Ta0kly"
+ConfigACS.ExplosionLogWebhook = "a"
+
+ConfigACS.LogBanWebhook = "https://discord.com/api/webhooks/925808944499290143/GMYxW-i2gBCnHusHNxVDg1WBFOYXbD04GLt2FhMg7B5ViFqhHZ3yQXSwM5ZG6-Ta0kly" --YOUR BANS/LOGS WEBHOOK
+ConfigACS.AntiVPNWebhook = "https://discord.com/api/webhooks/925808944499290143/GMYxW-i2gBCnHusHNxVDg1WBFOYXbD04GLt2FhMg7B5ViFqhHZ3yQXSwM5ZG6-Ta0kly" -- ANTIVPN WEBHOOK
+
+ConfigACS.ServerName = "https://discord.com/api/webhooks/925808944499290143/GMYxW-i2gBCnHusHNxVDg1WBFOYXbD04GLt2FhMg7B5ViFqhHZ3yQXSwM5ZG6-Ta0kly" --YOUR SERVER NAME
+
+ConfigACS.Version = "v1.6.4" -- DONT TOUCH THIS
+
+ConfigACS.License = "CRACKED"
